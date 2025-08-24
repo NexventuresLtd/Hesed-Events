ec58b96a60dccfb67aa65f98cc88d6f07dfed6e8
amuhirwa <a.muhirwa@alustudent.com>
2025-08-24 12:39:40 +0200

    change logo

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -167,6 +167,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite dev server
     "http://127.0.0.1:5173",
+    "https://hesed-events.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
